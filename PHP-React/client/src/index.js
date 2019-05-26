6c8cc1878ba27dc90aab4bd803d41a0f08c2e5ea
Gena <gershm@omc.co.il>
2019-05-26 10:24:59 +0300

    Adding basic routing and .gitignore file

--- PHP-React/client/src/index.js
+++ PHP-React/client/src/index.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 // Import bootstrap css
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-// function buildRoutes() {
-//     return (
-//         <Router history={browserHistory}>
-//             <Route path="/login" component={App}/>
-//         </Router>
-
-//     )
-// }
-ReactDOM.render(App(), document.getElementById("root"));
+function buildRoutes() {
+    return (
+        <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about/">About</Link>
+            </li>
+            <li>
+              <Link to="/users/">Users</Link>
+            </li>
+          </ul>
+        </nav>
+        <Route path="/users" exact component={App} />
+      </div>
+    </Router>
+    )
+}
+ReactDOM.render(buildRoutes(), document.getElementById("root"));
 
